eec150eb1a562d42ef0ef6d456a9612ebd27f972
boriAjDev <141878087+boriAjDev@users.noreply.github.com>
2023-10-03 12:08:02 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,7 +2,13 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = ">=3.0.0"
     }
   }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  use_oidc = true
+  features {}
 }
